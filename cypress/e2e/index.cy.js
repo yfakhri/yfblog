4f24a493dde577f6adadb0255ffcc68f8682a70d
Yusuf Fakhri <y.fakhri12@gmail.com>
2023-08-13 16:53:33 +0700

    update test

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,6 +1,6 @@
 it("titles are correct", () => {
   const page = cy.visit("/");
 
-  page.get("title").should("have.text", "Astro is awesome!");
-  page.get("h1").should("have.text", "Hello world from Astro");
+  page.get("title").should("have.text", "Astro");
+  page.get("h1").should("have.text", "Astro");
 });
