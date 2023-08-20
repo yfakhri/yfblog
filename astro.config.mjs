a01e6ce8040f314c741cd447b30092d0ffdc8d15
Yusuf Fakhri <y.fakhri12@gmail.com>
2023-08-20 20:18:53 +0700

    initial template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://yfblog.my.id/",
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    sitemap(),
+  ],
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
 });
--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,6 +1,6 @@
 it("titles are correct", () => {
   const page = cy.visit("/");
 
-  page.get("title").should("have.text", "Astro");
-  page.get("h1").should("have.text", "Astro");
+  page.get("title").should("have.text", "Yusuf Fakhri - Software Engineer");
+  page.get("h1").should("have.text", "Hi, I'm Yusuf");
 });
